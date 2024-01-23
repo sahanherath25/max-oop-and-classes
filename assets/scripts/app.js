5a94eb5d05b27528dcd764bbe970b3db78971dfa
Sahan Udara <sahanherath444@gmail.com>
2024-01-23 22:21:57 +0530

    Branch Created

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,13 +6,3 @@ const [first,second,...others]=foodArray;
 console.log(first)
 console.log(second)
 console.log(others)
-
-
-// const test = [ "Pasta", "Salad"];
-//
-// let [main,secondary]=test;
-// console.log(main,secondary)
-//
-// [main,secondary]=[secondary,main]
-//
-// console.log(main,secondary)
